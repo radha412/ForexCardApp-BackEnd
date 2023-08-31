3deaed706c324997064338bf912b464ed70a952a
radha412 <100402853+radha412@users.noreply.github.com>
2023-08-31 17:40:41 +0530

    save2

--- src/main/java/com/card/forexapp/repository/PotencialCustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.card.forexapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.card.forexapp.entity.PotencialCustomers;
-
-public interface PotencialCustomerRepository extends JpaRepository<PotencialCustomers, Long> {
-
-}
